f1a98e1f40ee7232fefbe45623c12665bf2abbb2
mutko <branko028@yahoo.com>
2019-03-12 17:14:29 +0100

    Styles and images

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
 const url = 'https://baza-filmova.herokuapp.com/filmovi/';
 
+let searchField = document.getElementById('search');
+let spaceForMovies = document.getElementById('filmovi');
+
 let godina;
 let naziv;
 let slika;
-let id;
 
+let allMovies = [];
+let searchResults = [];
+
+function showMovies(podatak) {
+    
+    let patternForMovies = ``;
+
+    for (let i = 0; i < podatak.length; i++) {         
+
+        godina = podatak[i].godina;
+        naziv = podatak[i].naziv;
+        slika = podatak[i].slika;
+
+        patternForMovies += `
+            <div class="movie">
+                <img src=${slika} alt="Movie poster" class="movie-img">
+                <div class="movie-body">
+                    <h3>${naziv}</h3>
+                    <p>${godina}</p>
+                </div>
+            </div>
+        `;
+
+    }
+    spaceForMovies.innerHTML = patternForMovies;
+}
 fetch(url)
     .then( response => response.json() )
-    .then( function(response) {
-        console.log(response);
+    .then( response => {
+        allMovies = searchResults = response;
+        showMovies(response)
+    })
 
-        let spaceForMovies = document.getElementById('filmovi');
-        let patternForMovies = ``;
-
-        for (let i = 0; i < response.length; i++) {         
-
-            godina = response[i].godina;
-            naziv = response[i].naziv;
-            slika = response[i].slika;
-            id = response[i].id;
-
-            patternForMovies += `
-                <div class="movie">
-                    <img src=${slika} alt="Movie poster" class="movie-img">
-                    <div class="movie-body">
-                        <h3>${naziv}</h3>
-                        <p>${godina}</p>
-                        <p>${id}</p>
-                    </div>
-                </div>
-            `;
-
-            console.log(patternForMovies);
-        }
-        spaceForMovies.innerHTML = patternForMovies;
-    } )
+searchField.addEventListener('input', function() {
+    searchResults = allMovies.filter(film => film.naziv.includes(searchField.value))
+    showMovies(searchResults)
+})
